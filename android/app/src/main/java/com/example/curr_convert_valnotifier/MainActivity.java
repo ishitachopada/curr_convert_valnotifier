bb1f9a3d2f64d5bf1c68edab841f48fd9179b099
vadiyagnik <vadiyagnik@gmail.com>
2022-11-25 11:01:28 +0530

    Initial commit

---
+++ android/app/src/main/java/com/example/curr_convert_valnotifier/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.curr_convert_valnotifier;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'first.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: first(),
+  ));
+}
+
+// http
+// currency_picker
